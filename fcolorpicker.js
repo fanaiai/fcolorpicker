9e7174b7acb3914bbff72c14c274d50d8d60342e
Fanaiai <516039800@qq.com>
2019-12-16 15:51:21 +0800

    11

--- fcolorpicker.js
+++ fcolorpicker.js
@@ -481,15 +481,49 @@ dynamicLoadCss(csslist);
                     return ele;
                 }
             })
+            this.curcolordom.style.background=this.color.rgba;
+
+            if(!this.dom){
+                return;
+            }
             this.dom.querySelector(".current-color").style.background = this.color.rgba;
             this.dom.querySelector(".current-color-value input").value = this.color[this.option.format];
-            this.curcolordom.style.background=this.color.rgba;
             this.setBarPos();
         },
         setBarPos: function () {
             this.opacitybar.style.top = (1 - this.color.rgbav[3]) * 100 + "%";
             if(this.color.hslav[1]!=0){
             this.huebar.style.top = (this.color.hslav[0]*100)/360 + "%";}
+        },
+        $copy : function(text) {
+            // if (text.indexOf('-') !== -1) {
+            //     let arr = text.split('-');
+            //     text = arr[0] + arr[1];
+            // }
+            var textArea = document.createElement("textarea");
+            textArea.style.position = 'fixed';
+            textArea.style.top = '0';
+            textArea.style.left = '0';
+            textArea.style.width = '2em';
+            textArea.style.height = '2em';
+            textArea.style.padding = '0';
+            textArea.style.border = 'none';
+            textArea.style.outline = 'none';
+            textArea.style.boxShadow = 'none';
+            textArea.style.background = 'transparent';
+            textArea.value = text;
+            document.body.appendChild(textArea);
+            textArea.select();
+
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? '成功复制到剪贴板' : '该浏览器不支持点击复制到剪贴板';
+                layer.msg(msg,{skin:"suclayer"});
+            } catch (err) {
+                layer.msg('该浏览器不支持点击复制到剪贴板',{skin:"errorlayer"});
+            }
+
+            document.body.removeChild(textArea);
         }
     }
     window.FColorPicker = FColorPicker;
